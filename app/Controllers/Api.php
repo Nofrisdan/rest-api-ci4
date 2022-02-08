f5d09b0ca893000054920eb363804feb1339e84b
Nofrisdan <nofrisdansitopu22@gmail.com>
2022-02-08 15:40:33 +0700

    Menambahkkan controller create

--- app/Controllers/Api.php
+++ app/Controllers/Api.php
@@ -15,11 +15,12 @@ use App\Libraries\Respon;
 class Api extends BaseController
 {
     use ResponseTrait;
-    protected $tb_user, $lib;
+    protected $tb_user, $lib, $validasi;
     public function __construct()
     {
         $this->tb_user = new Tb_user();
         $this->lib = new Respon();
+        $this->validasi = service("validation");
     }
 
 
@@ -43,5 +44,46 @@ class Api extends BaseController
 
 
     // ============= CREATE ==================
+    public function create()
+    {
 
+        // validation
+        if (!$this->validate([
+            "nama_user" => [
+                "rules" => "required",
+                "errors" => [
+                    "required" => "Harap Masukkan Parameter Nama User"
+                ]
+            ],
+            "email" => [
+                "rules" => "required|is_unique[tb_user.email]",
+                "errors" => [
+                    "required" => "Harap Masukkan Parameter email",
+                    "is_unique" => "Alamat Email Yang anda masukkan sudah terdaftar"
+
+                ]
+            ],
+            "wa" => [
+                "rules" => "required",
+                "errors" => [
+                    "required" => "Harap Masukkan parameter wa"
+                ]
+            ],
+            "password" => [
+                "rules" => "required",
+                "errors" => [
+                    "required" => "Harap Masukkan Parameter Password"
+                ]
+            ],
+        ])) {
+
+            $data = $this->lib->Error($this->validasi->getErrors());
+
+            return  $this->respond($data);
+        }
+
+        $data = $this->lib->success($this->request->getVar());
+
+        return $this->respond($data);
+    }
 }
